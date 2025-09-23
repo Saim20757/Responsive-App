50c69cae4c751e6f81887358d9958bdb6dc1cdb2
Saim20757 <hmsaim546@gmail.com>
2025-09-23 17:01:16 +0600

    Sign - up Page Complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
